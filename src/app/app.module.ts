ce7a70877abff0ea525544c564f38d1c38a3e8d7
Izuchukwu Samson <izuchukwusamson5@gmail.com>
2023-07-15 16:37:58 +0100

    chore: tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    AboutComponent,
+    FooterComponent,
+    HeaderComponent,
+    AddTaskComponent,
+    ButtonComponent,
+    TaskItemComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
